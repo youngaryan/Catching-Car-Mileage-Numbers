c9a229507187a5e72e74b3f46b3929d93fddc614
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-03-08 18:21:38 +0000

    works fine for returning 2 however it doesn't work well for retuening 1 for the pal function

--- mileNew.java
+++ mileNew.java
@@ -1,7 +1,8 @@
+//https://www.codewars.com/kata/52c4dd683bfd3b434c000292/train/java
 public class mileNew {
 
     public static void main(String[] args) {
-        System.out.println(isInteresting(541,new int[]{1337, 256}));
+        System.out.println(isInteresting(3442,new int[]{1337, 256}));
     }
     //return 2 if it matches
     //return 1 if its 2 miles away from matching it
@@ -11,7 +12,8 @@ public class mileNew {
         //return followedByAllZeros(number);
         //return everyDigitSameNumber(number);
         //return incrementing(number);
-        return decrementing(number);
+        //return decrementing(number);
+        return  palindrome(number);
     }
     //followedByAllZeros is Done.
     public static int followedByAllZeros(int number){
@@ -192,8 +194,95 @@ public class mileNew {
             return 0;
         }
     }
-    public static int palindrome(int number){
-        return 0;
+    public static int palindrome(int number) {
+        boolean two = false;
+        boolean one = false;
+        String numStr = Integer.toString(number);
+
+        if (numStr.length() % 2 == 0) {
+            char[] evenNumArrFirst = new char[numStr.length() ];
+            char[] evenNumArrSecond = new char[numStr.length() ];
+
+            for (int i = 0; i < numStr.length() / 2; i++) {
+                evenNumArrFirst[i] = numStr.charAt(i);
+            }
+            for (int i = numStr.length() - 1; i >= numStr.length() / 2; i--) {
+                evenNumArrSecond[numStr.length() - 1 - i] = numStr.charAt(i);
+            }
+            for (int i = 0; i < numStr.length() / 2; i++) {
+                if (evenNumArrFirst[i] != evenNumArrSecond[i]) {
+                    two = false;
+                    break;
+                } else {
+                    two = true;
+                }
+            }
+
+            if (!two) {
+                boolean initial = false;
+
+                for (int i = 1; i < numStr.length() / 2 - 1; i++) {
+                    if (evenNumArrFirst[i] != evenNumArrSecond[i]) {
+                        initial = false;
+                        break;
+                    } else {
+                        initial = true;
+                    }
+                }
+                if (initial && (evenNumArrFirst[0] <= evenNumArrFirst[numStr.length() / 2 -1] + 2)) {
+                    one = true;
+                }
+
+            }
+
+        } else {
+
+            char[] oddNumArrFirst = new char[numStr.length() ];
+            char[] oddNumArrSecond = new char[numStr.length() ];
+
+            for (int i = 0; i < numStr.length() / 2; i++) {
+                oddNumArrFirst[i] = numStr.charAt(i);
+            }
+            for (int i = numStr.length() - 1; i >= numStr.length() / 2; i--) {
+                oddNumArrSecond[numStr.length() - 1 - i] = numStr.charAt(i);
+            }
+            for (int i = 0; i < numStr.length() / 2; i++) {
+                if (oddNumArrFirst[i] != oddNumArrSecond[i]) {
+                    two = false;
+                    break;
+                } else {
+                    two = true;
+                }
+            }
+
+            if (!two) {
+                boolean initial = false;
+
+                for (int i = 1; i < numStr.length() / 2 ; i++) {
+                    if (oddNumArrFirst[i] != oddNumArrSecond[i]) {
+                        initial = false;
+                        break;
+                    } else {
+                        initial = true;
+                    }
+                }
+                if (initial && (oddNumArrFirst[0] <= oddNumArrSecond[oddNumArrSecond.length-1] + 2)) {
+                    one = true;
+                }
+
+
+
+
+            }
+
+        }
+        if (two) {
+            return 2;
+        } else if (one) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
     public static int matchAw (int number, int[] awesomePhrases){
         return 0;
